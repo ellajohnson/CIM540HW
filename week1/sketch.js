7f22ff1367337cdbca6036fc3ed76deaed113e00
Ella Johnson <exj112@miami.edu>
2018-08-22 13:25:59 -0400

    Test for homework assignment

---
+++ week1/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(800, 400);
+  background(204);
+
+}
+
+function draw() {
+  //head
+  ellipse(300, 125, 200, 85);
+  //mouth
+  ellipse(300, 140, 100, 25);
+  //left eye
+  ellipse(260, 75, 20, 20);
+  //right eye
+  ellipse(410, 120, 20, 20);
+}
