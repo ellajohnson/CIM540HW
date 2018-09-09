7b6956e2d3b40b4714c74445f0c35362c44aee32
Ella Johnson <exj112@miami.edu>
2018-09-09 13:43:21 -0400

    Official HW

--- week 4/sketch.js
+++ week 4/sketch.js
@@ -1,7 +1,7 @@
-var x = 150
-var y = 50
-var d = 50
-var s = 30
+var x = 150;
+var y = 50;
+var d = 50;
+var s = 30;
 function setup() {
   createCanvas(800, 400);
   background(204);
@@ -12,5 +12,5 @@ function draw() {
   rect(x, y, s, s);
   rect(x + s + d, y, 2 * s, 2 * s);
   rect((x + s +d) + (2 * s) +d, y, 3 * s, 3 * s);
-  
+
 }
