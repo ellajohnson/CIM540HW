fc6943ad80f368d2c3c5050c848a64c78f73495f
Ella Johnson <exj112@miami.edu>
2018-09-09 13:40:27 -0400

    Official Assignment

---
+++ week 4/sketch.js
@@ -0,0 +1,16 @@
+var x = 150
+var y = 50
+var d = 50
+var s = 30
+function setup() {
+  createCanvas(800, 400);
+  background(204);
+
+}
+
+function draw() {
+  rect(x, y, s, s);
+  rect(x + s + d, y, 2 * s, 2 * s);
+  rect((x + s +d) + (2 * s) +d, y, 3 * s, 3 * s);
+  
+}
